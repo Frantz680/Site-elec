2fd60f1d45fa19b2882b81dd6accaa2e67c06b1e
Frantz680 <dubdub68250@hotmail.fr>
2021-06-20 19:28:06 +0200

    Venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ACER\OneDrive\Documents\Fichiers\Git\Mon dépot\Elec\Elec\.venv
+set VIRTUAL_ENV=C:\Users\ACER\OneDrive\Documents\Fichiers\Git\Mon dépot\Site-elec\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
