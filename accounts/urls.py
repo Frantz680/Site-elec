e4a7e1c06744566869a2764d4dca8d393322e6ab
Frantz680 <dubdub68250@hotmail.fr>
2021-06-21 07:25:28 +0200

    Ajout:urls, templates base, login, logout,register

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from django.contrib.auth import views
+from .forms import LoginForm
+from accounts.views import my_account_views
+
+urlpatterns = [
+    path('login/', views.LoginView.as_view(
+        template_name='accounts/login.html',
+        redirect_authenticated_user=True,
+        authentication_form=LoginForm), name="login"),
+
+    path('logout/', views.LogoutView.as_view(
+        template_name='accounts/logout.html',
+        next_page='/accounts/login/'), name="logout"),
+
+    path('my_account/', my_account_views, name='my_account'),
+]
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication
+LOGIN_REDIRECT_URL = 'home'
