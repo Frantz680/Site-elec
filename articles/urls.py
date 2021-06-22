2d98c8f35bc88b53a77fbff6e318d3ae86ef220d
Frantz680 <dubdub68250@hotmail.fr>
2021-06-22 17:00:18 +0200

    Templete: password_reset

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from articles.views import home_tableau
+from articles.views import home_tableau, home
 
 
 urlpatterns = [
+    path('', home, name='home'),
     path('tableau_elec/', home_tableau, name='home_tableau'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+def home(request):
+    """
+    Show home page.
+    """
+
+    return render(request, "index.html")
+
+
 def home_tableau(request):
     """
     Display the 'legal notices' page.
