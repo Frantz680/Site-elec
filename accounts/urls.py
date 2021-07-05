93a853e3bc2af19725ecdbe8cb692a9535cee79e
Frantz680 <dubdub68250@hotmail.fr>
2021-07-05 10:14:00 +0200

    Fonctionnel page login, register

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,36 +4,37 @@ from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView
     PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetView, PasswordResetDoneView
 
 from .forms import LoginForm
-# from accounts.views import my_account_views
+from accounts.views import registrer_views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(
-        template_name='accounts/login.html',
+        template_name='registration/login.html',
         redirect_authenticated_user=True,
         authentication_form=LoginForm), name="login"),
 
     path('logout/', views.LogoutView.as_view(
-        template_name='accounts/logout.html',
-        next_page='/accounts/login/'), name="logout"),
+        template_name='registration/logout.html',
+        next_page='/registration/login/'), name="logout"),
 
+    path('register/', registrer_views, name='register'),
     # path('my_account/', my_account_views, name='my_account'),
 
-    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
     name='password_change_done'),
 
-    path('password_change/', PasswordChangeView.as_view(template_name='accounts/password_change.html'),
+    path('password_change/', PasswordChangeView.as_view(template_name='registration/password_change.html'),
         name='password_change'),
 
-    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
      name='password_reset_done'),
 
-    path('password_reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html',
+    path('password_reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html',
     success_url=reverse_lazy('password_reset_complete')),
      name='password_reset_confirm'),
 
-    path('password_reset/', PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
+    path('password_reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
      name='password_reset'),
 
-    path('password_reset_complete/', PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
      name='password_reset_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
+from .forms import RegisterForm
 # Create your views here.
+
+
+def registrer_views(request):
+    """
+    User registration.
+    :param request:
+    :return: Direction to main page or register.
+    """
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Votre compte a été crée avec succès.')
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})
