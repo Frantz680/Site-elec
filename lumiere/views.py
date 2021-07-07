3a363dd84cb9a712cb85f519bf007e73268140a3
Frantz680 <dubdub68250@hotmail.fr>
2021-07-07 17:24:13 +0200

    Commentaire fonctionnel

--- lumiere/views.py
+++ lumiere/views.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .forms import comment
+from .forms import CommentForm
 from .models import Comment, Article
 
 # Create your views here.
@@ -13,16 +13,16 @@ def home(request):
     Show home page.
     """
     comments = Comment.objects.all()
-    articles = Article.objects.all()
+    articles = Article.objects.all().order_by('date_publication')
 
     if request.method == 'POST':
-        form = comment(request.POST)
+        form = CommentForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Votre compte a été crée avec succès.')
             return redirect('home')
     else:
-        form = comment()
+        form = CommentForm()
     return render(request, 'home_lumiere/index.html', {'form': form, 'comments': comments, 'articles': articles})
 
 
